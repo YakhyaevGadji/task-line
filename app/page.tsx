6ca03a4647db94dd2a66230038255857570f6dc0
YakhyaevGadji <gadji180105@gmail.com>
2024-08-31 18:15:05 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/shared/components/ui/button';
+import { Input } from '@/shared/components/ui/input';
+
+export default function Home() {
+    return (
+        <main>
+            <Input value="test" />
+        </main>
+    );
+}
