673e85e802c7e5152773d634cef10ee4076fdfa1
Magomed Elmurzaev <ban0le4@gmail.com>
2024-09-02 20:48:16 +0300

    created regist

--- app/authentication/register/page.tsx
+++ app/authentication/register/page.tsx
@@ -8,11 +8,24 @@ import Link from 'next/link';
 const RegisterUser: React.FC = () => {
     return (
         <div className="mt-40 mx-auto w-[720px] shadow-xl rounded-md bg-white px-[140px] py-[90px]">
-            <Title className="font-semibold mb-4" text="Auth" size="lg" />
-
+            <Title className="font-semibold mb-4" text="Register" size="lg" />
+            <div className="flex gap-3">
+                <Input
+                    className="bg-[#F3F4F6] p-5 mb-4 rounded"
+                    placeholder="Name"
+                />
+                <Input
+                    className="bg-[#F3F4F6] p-5 mb-4 rounded"
+                    placeholder="LastName"
+                />
+            </div>
             <Input
                 className="bg-[#F3F4F6] p-5 mb-4 rounded"
                 placeholder="Login"
+            />
+            <Input
+                className="bg-[#F3F4F6] p-5 mb-4 rounded"
+                placeholder="Email"
             />
             <Input
                 className="bg-[#F3F4F6] p-5 mb-4 rounded"
@@ -20,8 +33,11 @@ const RegisterUser: React.FC = () => {
             />
             <Button className="flex mx-auto p-5 mb-4 rounded"> Register</Button>
             <p className="text-center text-[12px]">
-                Already have an account?{' '}
-                <Link className="text-slate-600" href="/authentication/login">
+                Already have an account?
+                <Link
+                    className="text-slate-600 pl-1"
+                    href="/authentication/login"
+                >
                     Sign in here.
                 </Link>
             </p>
